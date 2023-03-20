022ecddb1dcd58c454af3e0c79c227687bbdb3b8
Daisy Lagos <125144542+DaisyLagos@users.noreply.github.com>
2023-03-19 18:16:18 -0600

    Cambios realizados con carlos en paquetes y subP

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//document.getElementById("comprar").addEventListener("click", abrir);
+
+// const ventana = window.open("pago.html");
+
+
+ function abrir(){
+  location.href = "pago.html"; 
+
+
+}
+
+// var loca = 
+
+
+
+
+
+
+
+
+
+
+
